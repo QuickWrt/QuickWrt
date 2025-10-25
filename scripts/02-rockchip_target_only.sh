fed7ab989836860873e798e5403dd62073101fe5
OPPEN321 <zj18139624826@gmail.com>
2025-10-26 07:48:22 +0800

    Update 02-rockchip_target_only.sh

--- scripts/02-rockchip_target_only.sh
+++ scripts/02-rockchip_target_only.sh
@@ -3,8 +3,3 @@
 # 使用特定的优化
 sed -i 's,-mcpu=generic,-march=armv8-a+crc+crypto,g' include/target.mk
 sed -i 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
-
-#Vermagic
-wget https://downloads.openwrt.org/releases/$tag_version/targets/rockchip/armv8/profiles.json
-jq -r '.linux_kernel.vermagic' profiles.json >.vermagic
-sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
