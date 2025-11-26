640fe74124671fd772248aa15f49885cdb80a6e6
OPPEN321 <zj18139624826@gmail.com>
2025-11-26 21:03:37 +0800

    Update 04-fix_kmod.sh

--- scripts/04-fix_kmod.sh
+++ scripts/04-fix_kmod.sh
@@ -69,14 +69,18 @@ curl -s $mirror/openwrt/patch/packages-patches/batman-adv/901-fix-linux-6.12rc2-
 # xtables-addons module
 rm -rf feeds/packages/net/xtables-addons
 git clone https://$github/sbwml/kmod_packages_net_xtables-addons feeds/packages/net/xtables-addons
+
 # netatop
 sed -i 's/$(MAKE)/$(KERNEL_MAKE)/g' feeds/packages/admin/netatop/Makefile
 curl -s $mirror/openwrt/patch/packages-patches/clang/netatop/900-fix-build-with-clang.patch > feeds/packages/admin/netatop/patches/900-fix-build-with-clang.patch
+
 # dmx_usb_module
 rm -rf feeds/packages/libs/dmx_usb_module
 git clone https://$gitea/zhao/feeds_packages_libs_dmx_usb_module feeds/packages/libs/dmx_usb_module
+
 # macremapper
 curl -s $mirror/openwrt/patch/packages-patches/clang/macremapper/100-macremapper-fix-clang-build.patch | patch -p1
+
 # coova-chilli module
 rm -rf feeds/packages/net/coova-chilli
 git clone https://$github/sbwml/kmod_packages_net_coova-chilli feeds/packages/net/coova-chilli
